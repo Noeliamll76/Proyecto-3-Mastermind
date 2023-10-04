aac2691cbb4c5f70446e8fb0a93c5e17114c7a71
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-10-04 19:37:05 +0200

    perf. Arrangement of statements to keep the array of the move in hexadecimal

--- js/game.js
+++ js/game.js
@@ -4,140 +4,170 @@ const color1 = document.getElementById('color1')
 const color2 = document.getElementById('color2')
 const color3 = document.getElementById('color3')
 
-const coloresJugada = JSON.parse(localStorage.getItem('colores'))
-color0.style.backgroundColor=coloresJugada[0]
-color1.style.backgroundColor=coloresJugada[1]
-color2.style.backgroundColor=coloresJugada[2]
-color3.style.backgroundColor=coloresJugada[3]
+const coloresJugar = JSON.parse(localStorage.getItem('colores'))
+color0.style.backgroundColor = coloresJugar[0]
+color1.style.backgroundColor = coloresJugar[1]
+color2.style.backgroundColor = coloresJugar[2]
+color3.style.backgroundColor = coloresJugar[3]
+
+console.log(coloresJugar)
 
 
 //INICIAR JUGADA
 const colorSeleccionado = document.getElementsByClassName('color-seleccionado')
 
-let fila=1
+let fila = 1
 const jugada = [];
 
+
 colorSeleccionado[0].addEventListener('click', () => {
-    if (jugada.length===0){
+    if (jugada.length === 0) {
         console.log(colorSeleccionado[0].style.backgroundColor);
-        const element = document.getElementById('elemento1-'+fila)
-        element.style.backgroundColor=colorSeleccionado[0].style.backgroundColor
-        // console.log(window.getComputedStyle(element).backgroundColor)
-        jugada.push(colorSeleccionado[0].style.backgroundColor)
+        const element = document.getElementById('elemento0-' + fila)
+        const color = coloresJugar[0]
+        element.style.backgroundColor=color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===1){
+    else if (jugada.length === 1) {
         console.log(colorSeleccionado[0].style.backgroundColor);
-        const element = document.getElementById('elemento2-'+fila)
-        element.style.backgroundColor=colorSeleccionado[0].style.backgroundColor
-        jugada.push(colorSeleccionado[0].style.backgroundColor)
+        const element = document.getElementById('elemento1-' + fila)
+        const color = coloresJugar[0]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
+
     }
-    else if (jugada.length===2){
+    else if (jugada.length === 2) {
         console.log(colorSeleccionado[0].style.backgroundColor);
-        const element = document.getElementById('elemento3-'+fila)
-        element.style.backgroundColor=colorSeleccionado[0].style.backgroundColor
-        jugada.push(colorSeleccionado[0].style.backgroundColor)
+        const element = document.getElementById('elemento2-' + fila)
+        const color = coloresJugar[0]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
+
     }
-    else if (jugada.length===3){
+    else if (jugada.length === 3) {
         console.log(colorSeleccionado[0].style.backgroundColor);
-        const element = document.getElementById('elemento4-'+fila)
-        element.style.backgroundColor=colorSeleccionado[0].style.backgroundColor
-        jugada.push(colorSeleccionado[0].style.backgroundColor)
+        const element = document.getElementById('elemento3-' + fila)
+        const color = coloresJugar[0]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
     console.log(jugada)
-
 })
-   
+
 colorSeleccionado[1].addEventListener('click', () => {
-    
-    if (jugada.length===0){
+    if (jugada.length === 0) {
         console.log(colorSeleccionado[1].style.backgroundColor);
-        const element = document.getElementById('elemento1-'+fila)
-        element.style.backgroundColor=colorSeleccionado[1].style.backgroundColor
-        jugada.push(colorSeleccionado[1].style.backgroundColor)
+        const element = document.getElementById('elemento0-' + fila)
+        const color = coloresJugar[1]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===1){
+    else if (jugada.length === 1) {
         console.log(colorSeleccionado[1].style.backgroundColor);
-        const element = document.getElementById('elemento2-'+fila)
-        element.style.backgroundColor=colorSeleccionado[1].style.backgroundColor
-        jugada.push(colorSeleccionado[1].style.backgroundColor)
+        const element = document.getElementById('elemento1-' + fila)
+        const color = coloresJugar[1]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===2){
+    else if (jugada.length === 2) {
         console.log(colorSeleccionado[1].style.backgroundColor);
-        const element = document.getElementById('elemento3-'+fila)
-        element.style.backgroundColor=colorSeleccionado[1].style.backgroundColor
-        jugada.push(colorSeleccionado[1].style.backgroundColor)
+        const element = document.getElementById('elemento2-' + fila)
+        const color = coloresJugar[1]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===3){
+    else if (jugada.length === 3) {
         console.log(colorSeleccionado[1].style.backgroundColor);
-        const element = document.getElementById('elemento4-'+fila)
-        element.style.backgroundColor=colorSeleccionado[1].style.backgroundColor
-        jugada.push(colorSeleccionado[1].style.backgroundColor)
+        const element = document.getElementById('elemento3-' + fila)
+        const color = coloresJugar[1]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    
     console.log(jugada)
-
-
-
 })
 colorSeleccionado[2].addEventListener('click', () => {
-    if (jugada.length===0){
+    if (jugada.length === 0) {
         console.log(colorSeleccionado[2].style.backgroundColor);
-        const element = document.getElementById('elemento1-'+fila)
-        element.style.backgroundColor=colorSeleccionado[2].style.backgroundColor
-        jugada.push(colorSeleccionado[2].style.backgroundColor)
+        const element = document.getElementById('elemento0-' + fila)
+        const color = coloresJugar[2]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===1){
+    else if (jugada.length === 1) {
         console.log(colorSeleccionado[2].style.backgroundColor);
-        const element = document.getElementById('elemento2-'+fila)
-        element.style.backgroundColor=colorSeleccionado[2].style.backgroundColor
-        jugada.push(colorSeleccionado[2].style.backgroundColor)
+        const element = document.getElementById('elemento1-' + fila)
+        const color = coloresJugar[2]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===2){
+    else if (jugada.length === 2) {
         console.log(colorSeleccionado[2].style.backgroundColor);
-        const element = document.getElementById('elemento3-'+fila)
-        element.style.backgroundColor=colorSeleccionado[2].style.backgroundColor
-        jugada.push(colorSeleccionado[2].style.backgroundColor)
+        const element = document.getElementById('elemento2-' + fila)
+        const color = coloresJugar[2]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===3){
+    else if (jugada.length === 3) {
         console.log(colorSeleccionado[2].style.backgroundColor);
-        const element = document.getElementById('elemento4-'+fila)
-        element.style.backgroundColor=colorSeleccionado[2].style.backgroundColor
-        jugada.push(colorSeleccionado[2].style.backgroundColor)
+        const element = document.getElementById('elemento3-' + fila)
+        const color = coloresJugar[2]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    
     console.log(jugada)
-
-
-
 })
 colorSeleccionado[3].addEventListener('click', () => {
-    if (jugada.length===0){
+    if (jugada.length === 0) {
         console.log(colorSeleccionado[3].style.backgroundColor);
-        const element = document.getElementById('elemento1-'+fila)
-        element.style.backgroundColor=colorSeleccionado[3].style.backgroundColor
-        jugada.push(colorSeleccionado[3].style.backgroundColor)
+        const element = document.getElementById('elemento0-' + fila)
+        const color = coloresJugar[3]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===1){
+    else if (jugada.length === 1) {
         console.log(colorSeleccionado[3].style.backgroundColor);
-        const element = document.getElementById('elemento2-'+fila)
-        element.style.backgroundColor=colorSeleccionado[3].style.backgroundColor
-        jugada.push(colorSeleccionado[3].style.backgroundColor)
+        const element = document.getElementById('elemento1-' + fila)
+        const color = coloresJugar[3]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===2){
+    else if (jugada.length === 2) {
         console.log(colorSeleccionado[3].style.backgroundColor);
-        const element = document.getElementById('elemento3-'+fila)
-        element.style.backgroundColor=colorSeleccionado[3].style.backgroundColor
-        jugada.push(colorSeleccionado[3].style.backgroundColor)
+        const element = document.getElementById('elemento2-' + fila)
+        const color = coloresJugar[3]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    else if (jugada.length===3){
+    else if (jugada.length === 3) {
         console.log(colorSeleccionado[3].style.backgroundColor);
-        const element = document.getElementById('elemento4-'+fila)
-        element.style.backgroundColor=colorSeleccionado[3].style.backgroundColor
-        jugada.push(colorSeleccionado[3].style.backgroundColor)
+        const element = document.getElementById('elemento3-' + fila)
+        const color = coloresJugar[3]
+        element.style.backgroundColor = color
+        jugada.push(color)
+        console.log (jugada)
     }
-    
     console.log(jugada)
-
-
-
 })
+
+//COMPARAR JUGADA CON COMBINACIÓN GANADORA
+const combinacionGanadora = JSON.parse(localStorage.getItem('combinacionGanadora'))
+for (let i = 0; i < array.length; i++) {
+    if (jugada[i]===combinacionGanadora[i])
+    {};
+    
+}
